f31b04a7e1aeb8c606825c31744852b1bc1e6cc4
Tavi Nathanson <tavi.nathanson@gmail.com>
2025-08-26 11:41:17 -0400

    Bump version to 0.4.8

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="imgtsero",
-    version="0.4.6",
+    version="0.4.8",
     author="Tavi Nathanson",
     description="A Python library for downloading and working with IMGT/HLA data files",
     long_description=long_description,
